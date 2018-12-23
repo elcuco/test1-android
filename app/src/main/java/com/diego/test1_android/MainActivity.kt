04eebb1e316776d799620eb3267b5040c0685ea2
Diego Iastrubni <diegoiast@gmail.com>
2018-12-23 15:00:36 +0200

    Skeleton of the UI

--- app/src/main/java/com/diego/test1_android/MainActivity.kt
+++ app/src/main/java/com/diego/test1_android/MainActivity.kt
@@ -1,29 +1,64 @@
 package com.diego.test1_android
 
+import android.net.Uri
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
+import com.diego.test1_android.dummy.DummyContent
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+
+
+class MainActivity : AppCompatActivity(),
+        HomeFragment.OnFragmentInteractionListener,
+        RSSFragment.OnListFragmentInteractionListener {
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when (item.itemId) {
-            R.id.navigation_home -> {
-                message.setText(R.string.title_home)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_dashboard -> {
-                message.setText(R.string.title_dashboard)
-                return@OnNavigationItemSelectedListener true
-            }
-        }
-        false
+        return@OnNavigationItemSelectedListener displayView(item.itemId)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        displayView(R.id.navigation_home)
+    }
+
+    fun displayView(viewId: Int): Boolean {
+        var fragment: Fragment? = null
+        var title = getString(R.string.app_name)
+
+        when (viewId) {
+            R.id.navigation_home -> {
+                fragment = HomeFragment()
+                title = "Home"
+            }
+            R.id.navigation_dashboard -> {
+                fragment = RSSFragment()
+                title = "RSS"
+            }
+        }
+
+        if (supportActionBar != null) {
+            supportActionBar!!.setTitle(title)
+        }
+
+        if (fragment != null) {
+            val ft = supportFragmentManager.beginTransaction()
+            ft.replace(R.id.mainFrame, fragment)
+            ft.commit()
+            return true
+        }
+
+        return false
+    }
+
+    override fun onFragmentInteraction(uri: Uri) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onListFragmentInteraction(item: DummyContent.DummyItem?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
